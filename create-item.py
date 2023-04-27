deaccc7f2a5f8770b50cdb5c29aa7fd4c5eb5145
Scott Eissfeldt <scott@eissfeldt.com>
2023-04-27 16:29:41 -0700

    add new item using put_item

---
+++ create-item.py
@@ -0,0 +1,11 @@
+import boto3
+
+table.put_item(
+   Item={
+        'username': 'janedoe',
+        'first_name': 'Jane',
+        'last_name': 'Doe',
+        'age': 25,
+        'account_type': 'standard_user',
+    }
+)
